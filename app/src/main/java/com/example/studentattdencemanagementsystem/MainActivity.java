25b20c430f9060db81c1c3a192d1b33d6ce9a29a
Chathun <Chathunishara4@gmail.com>
2024-04-15 07:26:15 +0530

    2Day

--- app/src/main/java/com/example/studentattdencemanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/studentattdencemanagementsystem/MainActivity.java
@@ -2,15 +2,28 @@ package com.example.studentattdencemanagementsystem;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
+    RecyclerView recyclerView;
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<ClassItem> classItems=new ArrayList<>();
+
+    EditText class_edt;
+    EditText subject_edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +32,43 @@ public class MainActivity extends AppCompatActivity {
 
         fab=findViewById(R.id.fab_main);
         fab.setOnClickListener(v->showDialog());
+
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        classAdapter =new ClassAdapter(this,classItems);
+        recyclerView.setAdapter(classAdapter);
     }
 
     private void showDialog() {
         AlertDialog.Builder builder=new AlertDialog.Builder(this);
         View view = LayoutInflater.from(this).inflate(R.layout.class_dialog,null);
         builder.setView(view);
-        builder.create().show();
+        AlertDialog dialog=builder.create();
+        dialog.show();
+
+        class_edt=view.findViewById(R.id.class_edt);
+        subject_edt=view.findViewById(R.id.subject_edt);
+
+        Button cancel=view.findViewById(R.id.cancel_btn);
+        Button add=view.findViewById(R.id.add_btn);
+
+        cancel.setOnClickListener(v->dialog.dismiss());
+        add.setOnClickListener(v->{
+            addClass();
+            dialog.dismiss();
+        });
+
+
+
+    }
+
+    private void addClass() {
+        String className=class_edt.getText().toString();
+        String subjectName=subject_edt.getText().toString();
+        classItems.add(new ClassItem(className,subjectName));
+        classAdapter.notifyDataSetChanged();
     }
 
 
