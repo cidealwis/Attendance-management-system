469d282e832d299609fc0b0c4ba4e657d0640863
Chathun <Chathunishara4@gmail.com>
2024-04-15 21:26:56 +0530

    6Day

--- app/src/main/java/com/example/studentattdencemanagementsystem/MyDialog.java
+++ app/src/main/java/com/example/studentattdencemanagementsystem/MyDialog.java
@@ -15,6 +15,7 @@ import androidx.fragment.app.DialogFragment;
 
 public class MyDialog extends DialogFragment {
     public static final String CLASS_ADD_DIALOG="addClass";
+    public static final String STUDENT_ADD_DIALOG="addStudent";
 
    private OnClickListener listener;
     public interface OnClickListener{
@@ -30,7 +31,35 @@ public class MyDialog extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         Dialog dialog=null;
         if (getTag().equals(CLASS_ADD_DIALOG))dialog=getAddClassDialog();
+        if (getTag().equals(STUDENT_ADD_DIALOG))dialog=getAddStudentDialog();
         return dialog;
+    }
+
+    private Dialog getAddStudentDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog,null);
+        builder.setView(view);
+
+        TextView title=view.findViewById(R.id.titleDialog);
+        title.setText("Add New Student");
+
+        EditText roll_edt=view.findViewById(R.id.est01);
+        EditText name_edt=view.findViewById(R.id.edt02);
+
+        roll_edt.setHint("Roll");
+        name_edt.setHint("Name");
+        Button cancel=view.findViewById(R.id.cancel_btn);
+        Button add=view.findViewById(R.id.add_btn);
+
+        cancel.setOnClickListener(v->dismiss());
+        add.setOnClickListener(v->{
+            String roll=roll_edt.getText().toString();
+            String name=name_edt.getText().toString();
+            roll_edt.setText(String.valueOf(Integer.parseInt(roll)+1));
+            name_edt.setText("");
+            listener.onClick(roll,name);
+        });
+        return builder.create();
     }
 
     private Dialog getAddClassDialog() {
